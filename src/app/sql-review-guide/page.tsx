76dd63419c2202d470555bf383c8d91dd5b07812
Sasha Sushko <sasha@sushko.me>
2023-04-03 14:52:51 +0300

    feat: static pages drafts (#2)
    * feat: create static pages
    
    * fix: styles importing

---
+++ src/app/sql-review-guide/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1>SQL Review Guide</h1>;
+}
---
+++ src/lib/route.js
@@ -0,0 +1,14 @@
+const Route = {
+  INDEX: '/',
+  ABOUT: '/about',
+  USECASES: '/usecases',
+  PRICING: '/pricing',
+  SQL_REVIEW_GUIDE: '/sql-review-guide',
+  BYTEBASE_PLUS: '/bytebase-plus',
+  DATABASE_GLOSSARY: '/database-glossary',
+  CHANGELOG: '/changelog',
+  BLOG: '/blog',
+  DOCS: '/docs',
+}
+
+export default Route;
