18a53375fc42ca483272c30bb2a9a1ccd8ea4f1d
Thi Van Le <vannnyle@gmail.com>
2023-05-09 12:54:45 +0100

    feat: add seo data

--- src/app/sql-review-guide/page.tsx
+++ src/app/sql-review-guide/page.tsx
@@ -1,8 +1,13 @@
+import getMetadata from '@/utils/get-metadata';
+
 import GuideLayout from '@/components/pages/sql-review-guide/guide-layout';
 import Hero from '@/components/pages/sql-review-guide/hero';
 import Subscription from '@/components/shared/subscription';
 
 import { getGuidelineTemplateList, sqlReviewSchema } from '@/lib/api-sql-review';
+import SEO_DATA from '@/lib/seo-data';
+
+export const metadata = getMetadata(SEO_DATA.SQL_REVIEW_GUIDE);
 
 export default function SQLReviewPage() {
   const guidelineTemplateList = getGuidelineTemplateList();
