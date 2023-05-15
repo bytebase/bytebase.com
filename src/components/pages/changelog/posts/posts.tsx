cb14cfd727556db93b48e2b7d279933e2ee4e29d
americano98 <mr.nik.bazarov@gmail.com>
2023-05-15 13:27:14 +0300

    fix: semantic id, gaps

--- src/components/pages/changelog/posts/posts.tsx
+++ src/components/pages/changelog/posts/posts.tsx
@@ -13,13 +13,19 @@ const Posts = ({ posts }: PostsProps) => {
   return (
     <section className="posts mt-[60px] lg:mt-[52px] md:mt-10 sm:mt-8">
       <div className="container">
-        <ul className="relative flex flex-col gap-y-10 border-l border-gray-90 lg:gap-y-9 md:gap-y-16 sm:gap-y-7 sm:border-none">
+        <ul className="relative flex flex-col gap-y-6 border-l border-gray-90 sm:border-none">
           {posts.map(({ slug, title, author, published_at, timeToRead, content }, index) => {
             const date = new Date(published_at);
             const formattedDate = format(date, 'MMM dd, yyyy');
             return (
-              <li key={slug} id={slug} className={clsx(index !== 0 && 'pt-10 lg:pt-9 sm:pt-7')}>
-                <article className="gap-x-grid grid auto-rows-min grid-cols-12">
+              <li key={slug}>
+                <article
+                  id={slug}
+                  className={clsx(
+                    'gap-x-grid grid auto-rows-min grid-cols-12',
+                    index !== 0 && 'pt-14 lg:pt-12 md:pt-10 sm:pt-6',
+                  )}
+                >
                   <header
                     className="sticky top-[60px] col-span-3 flex h-min flex-col gap-y-2.5 pl-5 before:absolute  before:top-0 before:left-0 before:h-full
                       before:w-0.5 before:-translate-x-1/2 before:rounded-full before:bg-primary-1 lg:top-[52px] md:top-10 md:gap-y-2 md:pl-4 sm:relative sm:top-auto sm:col-span-full sm:pl-0 sm:before:hidden"
