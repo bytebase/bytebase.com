bf01fd65f322e3322fca634a97b056ecb87827d4
americano98 <mr.nik.bazarov@gmail.com>
2023-05-15 21:37:33 +0300

    feat: add glossary word layout and main content

---
+++ src/app/database-glossary/[slug]/page.tsx
@@ -0,0 +1,53 @@
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+
+import getMetadata from '@/utils/get-metadata';
+
+import WordArticle from '@/components/pages/database-glossary/word-article';
+
+import { getAllGlossaryPosts, getGlossaryBySlug } from '@/lib/api-glossary';
+import Route from '@/lib/route';
+
+export default function GlossaryWordPage({ params }: { params: { slug: string } }) {
+  const { slug } = params;
+
+  const post = getGlossaryBySlug(slug);
+
+  if (!post) return notFound();
+
+  return (
+    <div className="container gap-x-grid grid grid-cols-12">
+      <WordArticle post={post} />
+    </div>
+  );
+}
+
+export async function generateStaticParams() {
+  const posts = getAllGlossaryPosts();
+
+  const pages: { slug: string }[] = [];
+
+  posts.forEach(({ list }) => list.forEach(({ slug }) => pages.push({ slug: slug! })));
+
+  return pages;
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
+  const { slug } = params;
+
+  const post = getGlossaryBySlug(slug);
+
+  if (!post) return notFound();
+
+  const { name, description } = post;
+
+  return getMetadata({
+    title: name,
+    description: description.substring(0, 160),
+    pathname: `${Route.DATABASE_GLOSSARY}/${slug}/`,
+  });
+}
