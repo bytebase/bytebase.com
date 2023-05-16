2bab879935497eba406232ee54d9ba1cefa9c642
americano98 <mr.nik.bazarov@gmail.com>
2023-05-16 12:21:34 +0300

    fix: toc sticky

--- src/components/pages/blog/aside/aside.tsx
+++ src/components/pages/blog/aside/aside.tsx
@@ -13,10 +13,10 @@ type AsideProps = AuthorProps & {
 
 const Aside = ({ tocItems }: AsideProps) => {
   return (
-    <aside className="scrollbar-hidden lg:gap-x-grid col-span-3 ml-auto flex w-full flex-col gap-y-9 overflow-y-auto lg:static lg:col-span-full lg:mt-14 lg:grid lg:grid-cols-12 md:mt-10 sm:mt-8 sm:gap-y-4">
+    <aside className="scrollbar-hidden lg:gap-x-grid col-span-3 ml-auto flex w-full flex-col gap-y-9 lg:static lg:col-span-full lg:mt-14 lg:grid lg:grid-cols-12 md:mt-10 sm:mt-8 sm:gap-y-4">
       <WhatIsBytebase />
       {/* <Author author={author} /> */}
-      {tocItems.length > 0 && <TableOfContents items={tocItems} />}
+      {tocItems.length > 0 && <TableOfContents items={tocItems} className="sticky top-9" />}
     </aside>
   );
 };
