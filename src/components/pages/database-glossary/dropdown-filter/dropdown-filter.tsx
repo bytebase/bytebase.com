43ccb6a82f56ac4d20ca6bfdfaa10ce08e3dac61
americano98 <mr.nik.bazarov@gmail.com>
2023-05-18 15:05:34 +0300

    fix: click outside

--- src/components/pages/database-glossary/dropdown-filter/dropdown-filter.tsx
+++ src/components/pages/database-glossary/dropdown-filter/dropdown-filter.tsx
@@ -45,7 +45,7 @@ const DropdownFilter = ({
   setActiveFilters,
   toggleFilter,
 }: FilterProps & { setActiveFilters: (filters: string[]) => void }) => {
-  const categoryRef = useRef<HTMLFieldSetElement>(null);
+  const categoryRef = useRef<HTMLDivElement>(null);
   const [isCategoryOpen, setIsCategoryOpen] = useState(false);
 
   useClickOutside([categoryRef], () => {
@@ -60,11 +60,11 @@ const DropdownFilter = ({
           className,
         )}
       >
-        <fieldset className="relative z-10 w-full" ref={categoryRef}>
+        <fieldset className="relative z-10 w-full">
           <legend className="text-14 font-bold uppercase leading-none tracking-[0.025em] text-gray-15">
             {title}
           </legend>
-          <div className="relative mt-4">
+          <div className="relative mt-4" ref={categoryRef}>
             <button
               className={clsx(
                 'group flex h-10 w-full items-center justify-between overflow-hidden rounded-xl border border-gray-70 pl-5 pr-4',
@@ -95,50 +95,50 @@ const DropdownFilter = ({
                 <CloseIcon className="h-3.5 w-3.5" />
               </button>
             )}
+
+            <m.ul
+              className={clsx(
+                'absolute inset-x-0 flex flex-col gap-y-4 rounded-b-xl border border-t-0 border-gray-70 bg-white p-5 shadow-[0px_5px_15px_rgba(15,22,36,0.1)]',
+                isCategoryOpen ? 'pointer-events-auto' : 'pointer-events-none',
+              )}
+              initial="hidden"
+              animate={isCategoryOpen ? 'visible' : 'hidden'}
+              variants={variants}
+              transition={{
+                duration: 0.2,
+              }}
+            >
+              {fieldsList.map((filter) => {
+                const [name, count] = filter;
+                const key: string = name.toLocaleLowerCase() + '-' + count;
+
+                return (
+                  <li className="flex items-center gap-x-2" key={key}>
+                    <input
+                      className="h-4 w-4 appearance-none rounded-sm border border-gray-30 bg-center bg-no-repeat transition-colors duration-100 checked:border-primary-1 checked:bg-primary-1 checked:bg-[url('/images/check-checkbox.svg')]"
+                      type="checkbox"
+                      name={`${title.toLocaleLowerCase()}[]`}
+                      id={key}
+                      value={name}
+                      checked={activeFilters.includes(name)}
+                      onChange={() => toggleFilter(name)}
+                    />
+                    <label
+                      className="flex cursor-pointer items-center gap-x-2 font-medium text-gray-30"
+                      htmlFor={key}
+                    >
+                      {name}
+                      {count > 0 && (
+                        <span className="flex min-w-[20px] items-center justify-center rounded-full bg-gray-94 px-[5px] pt-1 pb-0.5 text-14 font-medium leading-none text-gray-30">
+                          {count}
+                        </span>
+                      )}
+                    </label>
+                  </li>
+                );
+              })}
+            </m.ul>
           </div>
-
-          <m.ul
-            className={clsx(
-              'absolute inset-x-0 flex flex-col gap-y-4 rounded-b-xl border border-t-0 border-gray-70 bg-white p-5 shadow-[0px_5px_15px_rgba(15,22,36,0.1)]',
-              isCategoryOpen ? 'pointer-events-auto' : 'pointer-events-none',
-            )}
-            initial="hidden"
-            animate={isCategoryOpen ? 'visible' : 'hidden'}
-            variants={variants}
-            transition={{
-              duration: 0.2,
-            }}
-          >
-            {fieldsList.map((filter) => {
-              const [name, count] = filter;
-              const key: string = name.toLocaleLowerCase() + '-' + count;
-
-              return (
-                <li className="flex items-center gap-x-2" key={key}>
-                  <input
-                    className="h-4 w-4 appearance-none rounded-sm border border-gray-30 bg-center bg-no-repeat transition-colors duration-100 checked:border-primary-1 checked:bg-primary-1 checked:bg-[url('/images/check-checkbox.svg')]"
-                    type="checkbox"
-                    name={`${title.toLocaleLowerCase()}[]`}
-                    id={key}
-                    value={name}
-                    checked={activeFilters.includes(name)}
-                    onChange={() => toggleFilter(name)}
-                  />
-                  <label
-                    className="flex cursor-pointer items-center gap-x-2 font-medium text-gray-30"
-                    htmlFor={key}
-                  >
-                    {name}
-                    {count > 0 && (
-                      <span className="flex min-w-[20px] items-center justify-center rounded-full bg-gray-94 px-[5px] pt-1 pb-0.5 text-14 font-medium leading-none text-gray-30">
-                        {count}
-                      </span>
-                    )}
-                  </label>
-                </li>
-              );
-            })}
-          </m.ul>
         </fieldset>
       </form>
     </LazyMotion>
