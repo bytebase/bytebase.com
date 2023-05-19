37c6ee581d2e1e567398838e2ef5496dfefd4ab6
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2023-05-19 14:47:40 +0800

    feat: add borders to pro pricing column

--- src/components/pages/pricing/table/plan-card/feature-list/feature-list.tsx
+++ src/components/pages/pricing/table/plan-card/feature-list/feature-list.tsx
@@ -46,10 +46,7 @@ const FeatureList = ({
 }: FeatureListProps) => (
   <div
     className={clsx(
-      'divide-y border-b pt-[84px] text-center first:pt-4 3xl:first:pt-[19px] xl:pt-[85px] md:first:pt-[23px] sm:first:pt-[37px]',
-      title === 'pro'
-        ? 'divide-transparent border-transparent'
-        : 'divide-y divide-black divide-opacity-10 border-b  border-black  border-opacity-10',
+      'divide-y divide-black divide-opacity-10 border-b border-black border-opacity-10 pt-[84px] text-center first:pt-4 3xl:first:pt-[19px] xl:pt-[85px] md:first:pt-[23px] sm:first:pt-[37px]',
       withLongTitle ? 'md:pt-[110px] sm:pt-[105px]' : 'sm:pt-[82.5px]',
       { 'last:border-b-0': isLastSection },
     )}
