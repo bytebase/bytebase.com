a3fb64ea048912c16d4922f36561630edc6c62a2
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2023-05-22 14:35:46 +0800

    fix: add hover states and transitions to green accordion

--- src/components/pages/home/promo-sql-editor/accordion/accordion.tsx
+++ src/components/pages/home/promo-sql-editor/accordion/accordion.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx';
 
+import ChevronIcon from '@/svgs/bold-chevron.inline.svg';
 import { AccordionData } from '../promo-sql-editor';
 
 // TODO: refactor markup for chevrons to span > image.svg
@@ -22,27 +23,29 @@ const Accordion = ({
           })}
           onClick={() => onChange(idx)}
         >
-          <p className="flex items-center gap-4 text-24 leading-extra-tight tracking-tight xl:text-20 xl:leading-tight xl:tracking-normal md:gap-3 md:text-18">
-            {activeIndex === idx ? (
-              <img
-                className="h-8 w-8 rounded-full shadow-[0_5px_10px_0_rgba(156,201,182,0.8)] md:h-7 md:w-7"
-                src="/images/page/main/accordion-opened.svg"
-                alt=""
-                width={32}
-                height={32}
-                loading="lazy"
+          <p className="group flex items-center gap-4 text-24 leading-extra-tight tracking-tight xl:text-20 xl:leading-tight xl:tracking-normal md:gap-3 md:text-18">
+            <div
+              className={clsx(
+                'relative h-8 w-8 rounded-full bg-secondary-2 shadow-[0_5px_10px_0_rgba(156,201,182,0.8)] transition-colors duration-200 md:h-7 md:w-7',
+                activeIndex !== idx && 'group-hover:bg-tones-deep-green-dark',
+                activeIndex === idx && 'bg-white',
+              )}
+            >
+              <ChevronIcon
+                className={clsx(
+                  'absolute top-1/2 left-1/2 h-3.5 w-5 -translate-x-1/2 -translate-y-1/2 text-white transition-[transform,colors] duration-200',
+                  activeIndex === idx && 'rotate-90 !text-gray-15',
+                )}
               />
-            ) : (
-              <img
-                className="h-8 w-8 md:h-7 md:w-7"
-                src="/images/page/main/accordion-closed-secondary-2.svg"
-                alt=""
-                width={32}
-                height={32}
-                loading="lazy"
-              />
-            )}
-            <b className="font-bold md:font-semibold">{title}</b>
+            </div>
+            <b
+              className={clsx(
+                'font-bold text-gray-15 transition-colors duration-200 md:font-semibold',
+                activeIndex !== idx && 'group-hover:text-tones-deep-green-dark',
+              )}
+            >
+              {title}
+            </b>
           </p>
           {activeIndex === idx && (
             <p className="pt-2 pl-12 text-16 xl:pt-1 xl:text-14 xl:leading-snug md:pt-1.5 md:pl-10">
