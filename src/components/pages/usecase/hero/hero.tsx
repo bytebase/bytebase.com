d225167184cc21b889acd628ba4e30a3d0e6be31
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2023-05-23 12:49:02 +0800

    fix: adaptive layout fixes

--- src/components/pages/usecase/hero/hero.tsx
+++ src/components/pages/usecase/hero/hero.tsx
@@ -12,15 +12,15 @@ import Image from 'next/image';
 
 const Hero = ({ title, description }: HeroProps) => {
   return (
-    <section className="col-span-6 col-start-4 pt-[136px] xl:col-span-8 xl:col-start-3 xl:pt-[120px] md:col-span-full md:pt-24">
+    <section className="col-span-6 col-start-4 flex flex-col pt-[136px] xl:col-span-8 xl:col-start-3 xl:pt-[120px] md:col-span-full md:pt-24">
       <h1 className="text-left font-title text-90 font-semibold leading-none xl:text-68 md:text-56 sm:text-48">
         {title}
       </h1>
-      <p className="font-regular mt-4 text-18 leading-normal xl:text-16 xl:leading-snug md:text-14 md:leading-tight sm:text-14">
+      <p className="font-regular mt-4 text-18 leading-normal xl:text-16 xl:leading-snug lg:mt-3 md:mt-2 md:text-14 md:leading-tight sm:text-14">
         {description}
       </p>
-      <Logos />
-      <div className="mt-11 flex items-center gap-9 2xl:gap-8 xl:mt-10 xl:gap-6 md:mt-7 sm:mt-6 sm:gap-3.5">
+      <Logos className="sm:order-2" />
+      <div className="mt-11 flex items-center gap-9 2xl:gap-8 xl:mt-10 md:mt-9 md:gap-5 sm:order-1 sm:mt-8">
         <Button
           href={Route.REQUEST_DEMO}
           theme="primary-filled"
@@ -36,11 +36,11 @@ const Hero = ({ title, description }: HeroProps) => {
         width={716}
         height={545}
         alt="Bytebase Database scheme"
-        className="mx-auto mt-20 sm:hidden"
+        className="mx-auto mt-20 lg:mt-16 md:mt-12 sm:hidden"
         loading="lazy"
       />
       <Image
-        className="mx-auto mt-12 hidden sm:block"
+        className="mx-auto mt-12 hidden sm:order-3 sm:block"
         src="/images/page/main/db-scheme.png"
         alt="Bytebase Database scheme"
         width={366}
