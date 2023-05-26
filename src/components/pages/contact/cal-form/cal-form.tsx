61856cf9fb5cee95f7c63c6ec096aa8641bec6a1
Sasha Sushko <sasha@sushko.me>
2023-05-26 14:07:34 +0500

    fix: cal.com embed

--- src/components/pages/contact/cal-form/cal-form.tsx
+++ src/components/pages/contact/cal-form/cal-form.tsx
@@ -1,23 +1,9 @@
 'use client';
 
-import Cal, { getCalApi } from '@calcom/embed-react';
-
-import { useEffect } from 'react';
+import Cal from '@calcom/embed-react';
 
 const CalForm = () => {
-  useEffect(() => {
-    (async function () {
-      const cal = await getCalApi();
-      cal('ui', { styles: { branding: { brandColor: '#5647EB' } }, hideEventTypeDetails: false });
-    })();
-  }, []);
-
-  return (
-    <Cal
-      calLink="tianzhou/bytebase-product-walkthrough"
-      style={{ width: '100%', height: '100%', overflow: 'scroll' }}
-    />
-  );
+  return <Cal calLink="tianzhou/bytebase-product-walkthrough" config={{ theme: 'light' }} />;
 };
 
 export default CalForm;
