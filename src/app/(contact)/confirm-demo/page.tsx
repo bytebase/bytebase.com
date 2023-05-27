112b8a9c49bd2f2c7500abc76db61adb29132faa
Tianzhou Chen <t@bytebase.com>
2023-05-28 00:39:29 +0800

    chore: seperate request demo and confirm demo

---
+++ src/app/(contact)/confirm-demo/page.tsx
@@ -0,0 +1,19 @@
+import getMetadata from '@/utils/get-metadata';
+
+import Logos from '@/components/pages/contact/logos';
+
+import SEO_DATA from '@/lib/seo-data';
+
+export const metadata = getMetadata(SEO_DATA.DEMO);
+
+export default function Page() {
+  return (
+    <div className="container flex flex-col items-center">
+      <h1 className="font-title text-80 font-semibold leading-none 2xl:text-64 lg:text-56 sm:text-40">
+        Demo Booking Confirmed!
+      </h1>
+      <p className="mt-3 text-18">Thanks for booking time with us. Talk to you soon!</p>
+      <Logos />
+    </div>
+  );
+}
