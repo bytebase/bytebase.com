fea5c00cad6e3664212770fae69d8ac0fca18fa3
Tianzhou Chen <t@bytebase.com>
2023-05-28 00:40:57 +0800

    chore: align confirm booking to the left

--- src/app/(contact)/confirm-demo/page.tsx
+++ src/app/(contact)/confirm-demo/page.tsx
@@ -8,7 +8,7 @@ export const metadata = getMetadata(SEO_DATA.DEMO);
 
 export default function Page() {
   return (
-    <div className="container flex flex-col items-center">
+    <div className="container flex flex-col">
       <h1 className="font-title text-80 font-semibold leading-none 2xl:text-64 lg:text-56 sm:text-40">
         Demo Booking Confirmed!
       </h1>
