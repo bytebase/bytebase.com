ff75bd00ad6153c52a99706cf016c66da61856f4
Nikita <105175118+nikvoblikov@users.noreply.github.com>
2023-05-31 14:50:14 +0300

    feat: updating home page paddings

--- src/components/pages/home/promo-security/promo-security.tsx
+++ src/components/pages/home/promo-security/promo-security.tsx
@@ -68,11 +68,11 @@ const PromoSecurity = () => {
           </LinkUnderlined>
         </div>
         <div
-          className="relative col-start-5 col-end-13 self-stretch pt-[95px] before:absolute before:bottom-0 before:top-0 before:right-[166px] before:w-[252px] before:bg-security 3xl:col-start-7 3xl:pt-[80px] 3xl:before:right-[104px] xl:col-start-6 xl:before:right-[106px] xl:before:w-[216px] md:col-auto md:-mx-7 md:pt-[49px] md:pb-[39px] md:before:right-0 md:before:top-auto md:before:h-[427px] md:before:w-full md:before:bg-security-phone sm:-mx-4 sm:pb-[32px] sm:pt-[31px] sm:before:h-[324px]"
+          className="relative col-start-5 col-end-13 self-stretch pt-[95px] before:absolute before:bottom-0 before:top-0 before:right-[166px] before:w-[252px] before:bg-security 3xl:col-start-7 3xl:pt-[80px] 3xl:before:right-[104px] 2xl:before:right-[130px] xl:col-start-6 xl:before:right-[106px] xl:before:w-[216px] md:col-auto md:-mx-7 md:pt-[49px] md:pb-[39px] md:before:right-0 md:before:top-auto md:before:h-[427px] md:before:w-full md:before:bg-security-phone sm:-mx-4 sm:pb-[32px] sm:pt-[31px] sm:before:h-[324px]"
           ref={animationRef}
         >
           <div
-            className="relative mr-[9px] ml-auto aspect-[1.0597014925] w-[710px] 3xl:-mr-12 2xl:mr-0 2xl:w-[690px] xl:w-[560px] lg:mx-auto lg:w-[530px] md:hidden"
+            className="relative mr-[9px] ml-auto aspect-[1.0597014925] w-[710px] 3xl:-mr-14 2xl:-mr-8 2xl:pr-4 xl:-mr-2 xl:w-[560px] lg:mx-auto lg:mr-0 lg:w-[530px] md:hidden"
             ref={setRiveRef}
           >
             {isIntersecting ? <RiveComponent /> : null}
