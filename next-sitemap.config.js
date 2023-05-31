e4789de0d327b7966cc493445d2cfdb1ccc77086
Sasha Sushko <sasha@sushko.me>
2023-05-31 10:31:51 +0300

    fix: enable autogeneration for robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_DEFAULT_SITE_URL || 'https://bytebase.com',
-  generateRobotsTxt: false,
+  generateRobotsTxt: true,
 };
