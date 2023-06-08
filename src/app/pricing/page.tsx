ed3d390b993f5900913327b0a89a4934149d51d8
Tianzhou Chen <t@bytebase.com>
2023-06-09 01:15:21 +0800

    chore: add logo to the bottom of pricing page

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -2,6 +2,7 @@ import getMetadata from '@/utils/get-metadata';
 
 import Hero from '@/components/pages/pricing/hero';
 import Table from '@/components/pages/pricing/table';
+import Logos from '@/components/pages/home/logos';
 
 import SEO_DATA from '@/lib/seo-data';
 
@@ -12,6 +13,7 @@ export default function Page() {
     <>
       <Hero />
       <Table />
+      <Logos />
     </>
   );
 }
