87ab72a52467c2443b1eea0dd8db42e2ada11f21
Sasha Sushko <sasha@sushko.me>
2023-06-08 15:00:33 +0300

    fix: docs crosslink design (#64)
    Will add some color accent for crosslinks

--- src/components/shared/content/doc-link-block/doc-link-block.tsx
+++ src/components/shared/content/doc-link-block/doc-link-block.tsx
@@ -1,17 +1,18 @@
-import Link from '@/components/shared/link';
-
-import BookOpenIcon from '@/svgs/book-open.inline.svg';
+import Button from '@/components/shared/button';
+import ArrowIcon from '@/svgs/arrow.inline.svg';
 
 const DocLinkBlock = ({ url, title }: { url: string; title: string }) => (
-  <figure className="not-prose">
-    <Link
-      className="doc-link-block flex items-center justify-center rounded-[70px] border-2 border-gray-90 p-4 text-13 font-bold uppercase leading-none tracking-wide hover:border-gray-60 active:border-gray-60"
+  <div className="not-prose group my-11">
+    <Button
       href={url}
+      theme="primary-outline"
+      className="inline-flex h-auto min-h-[52px] w-full items-center gap-2 !py-2 text-center"
+      size="md"
     >
-      <BookOpenIcon className="mr-2 h-4 w-4" />
-      <span>{title}</span>
-    </Link>
-  </figure>
+      {title}
+      <ArrowIcon className="h-2.5 w-4 shrink-0 transition-transform duration-200 group-hover:translate-x-1" />
+    </Button>
+  </div>
 );
 
 export default DocLinkBlock;
