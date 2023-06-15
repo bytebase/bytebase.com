66b9928abdc909401a1b1c3a761c84f373852801
Adela <a@bytebase.com>
2023-06-15 14:06:50 +0800

    Update src/components/pages/contact/logos/logos.tsx

--- src/components/pages/contact/logos/logos.tsx
+++ src/components/pages/contact/logos/logos.tsx
@@ -4,7 +4,7 @@ const Logos = () => {
   return (
     <div className="col-span-5 mt-9 border-t border-gray-90 pt-9 lg:col-span-full lg:mt-0 lg:border-none lg:pt-0">
       <p className="text-18 leading-extra-tight text-gray-40">Trusted by engineers worldwide</p>
-      <ul className="it mt-7 flex flex-wrap items-start gap-x-9 gap-y-5 2xl:gap-x-7 xl:gap-x-9 md:gap-x-6 xs:gap-x-5 xs:gap-y-6">
+      <ul className="it mt-7 flex flex-wrap items-start gap-y-5 gap-x-9 2xl:gap-x-7 xl:gap-x-9 md:gap-x-6 xs:gap-x-5 xs:gap-y-6">
         {LOGO_LIST.map((logo, idx) => (
           <li key={idx} className={logo.mobileOrderClassName}>
             {/* eslint-disable-next-line jsx-a11y/alt-text */}
