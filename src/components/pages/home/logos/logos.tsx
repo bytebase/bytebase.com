a2cb2e9949e8175f4d633d9320bc23dfc57d1ca5
Adela <a@bytebase.com>
2023-06-15 14:01:23 +0800

    Update src/components/pages/home/logos/logos.tsx

--- src/components/pages/home/logos/logos.tsx
+++ src/components/pages/home/logos/logos.tsx
@@ -9,7 +9,7 @@ const Logos = () => {
         <h2 className="max-w-3xl text-center font-title text-72 font-semibold leading-none xl:max-w-2xl xl:text-56 md:max-w-lg md:text-48 sm:text-40">
           Trusted by engineers worldwide
         </h2>
-        <ul className="mt-16 grid max-w-[1133px] grid-cols-[repeat(4,auto)] flex-wrap justify-center gap-x-24 gap-y-12 2xl:mt-14 xl:mt-12 xl:max-w-[740px] xl:gap-x-16 xl:gap-y-8 md:mt-10 md:max-w-[580px] md:gap-x-10 md:gap-y-7 sm:mt-8 xs:flex xs:max-w-[460px] xs:flex-wrap xs:justify-center xs:gap-x-5 xs:gap-y-6">
+        <ul className="mt-16 grid max-w-[1133px] grid-cols-[repeat(4,auto)] flex-wrap justify-center gap-x-24 gap-y-12 2xl:mt-14 xl:mt-12 xl:max-w-[740px] xl:gap-y-8 xl:gap-x-16 md:mt-10 md:max-w-[580px] md:gap-y-7 md:gap-x-10 sm:mt-8 xs:flex xs:max-w-[460px] xs:flex-wrap xs:justify-center xs:gap-x-5 xs:gap-y-6">
           {LOGO_LIST.map((logo) => (
             <li key={logo.alt} className={clsx('flex items-center', logo.mobileOrderClassName)}>
               <img
