182b1cec7bf3597cf0300a0016d8b8fdb4c0cbcf
Tianzhou Chen <t@bytebase.com>
2023-06-17 17:12:34 +0800

    chore: change what is bytebase CTA to pricing page

--- src/components/pages/blog/aside/what-is-bytebase/what-is-bytebase.tsx
+++ src/components/pages/blog/aside/what-is-bytebase/what-is-bytebase.tsx
@@ -6,7 +6,7 @@ const WhatIsBytebase = () => {
   return (
     <Link
       className="flex flex-col justify-between rounded-xl bg-primary-1 p-5 text-white hover:bg-primary-2 lg:col-span-5 lg:col-start-2 md:col-span-6 sm:col-span-full sm:rounded-[4px]"
-      href={ROUTE.ABOUT}
+      href={ROUTE.PRICING}
     >
       <div>
         <h3 className="font-title text-34 leading-none">What is Bytebase?</h3>
@@ -16,7 +16,7 @@ const WhatIsBytebase = () => {
         </p>
       </div>
       <span className="mt-4 flex h-9 w-[122px] items-center justify-center rounded-full bg-gray-15 text-13 font-bold uppercase tracking-wide text-white">
-        Learn more
+        See Features
       </span>
     </Link>
   );
