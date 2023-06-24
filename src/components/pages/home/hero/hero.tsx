178adcdbdd26d5047ce9abe90dbb941d8bb1b857
Tianzhou Chen <t@bytebase.com>
2023-06-24 16:20:57 +0800

    chore: more explicit what’s new

--- src/components/pages/home/hero/hero.tsx
+++ src/components/pages/home/hero/hero.tsx
@@ -140,7 +140,7 @@ const Hero = ({ latestVersion }: { latestVersion: { number: string; slug: string
                   What’s new?
                 </span>
                 <span className="flex items-center gap-1.5 px-2">
-                  {latestVersion.number} Released
+                  Release {latestVersion.number} - SQL lint for Snowflake
                   <SmallArrowIcon width={7} height={6} />
                 </span>
               </span>
