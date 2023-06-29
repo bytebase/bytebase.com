b2148a0799775bf8a63680a8f5fcbfa5ea324eb9
Tianzhou Chen <t@bytebase.com>
2023-06-29 10:03:46 +0800

    chore: add Reddit pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,6 +56,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           `,
           }}
         />
+        <Script
+          id="ga"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+!function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','t2_eaojgtkcg', {"optOut":false,"useDecimalCurrencyValues":true});rdt('track', 'PageVisit');
+`,
+          }}
+        />
         {topBanner && <Banner bannerText={topBanner.title} bannerUrl={topBanner.pathname} />}
         <div className="relative flex min-h-screen flex-col">
           <Header hasBanner />
