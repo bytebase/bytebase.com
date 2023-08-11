dbf3b19a38af38a1dd32c51539df4ea2f2442d04
steven <stevenlgtm@gmail.com>
2023-08-11 14:03:46 +0800

    chore: add share social links into blog aside

--- src/components/pages/blog/aside/social-links/social-links.tsx
+++ src/components/pages/blog/aside/social-links/social-links.tsx
@@ -1,23 +1,41 @@
 import Link from '@/components/shared/link';
 
-import LinkedinIcon from '@/svgs/linkedin.inline.svg';
 import TwitterIcon from '@/svgs/twitter.inline.svg';
+import LinkedinIcon from '@/svgs/linkedin.inline.svg';
+import HackerNewsIcon from '@/svgs/hackernews.inline.svg';
 import clsx from 'clsx';
 
 const icons = {
   twitter: {
     Icon: TwitterIcon,
     className: 'text-secondary-3',
+    iconClassName: 'h-6 w-6',
+    getShareUrl: (text: string, url: string) => {
+      return `https://twitter.com/share?text=${text}&url=${url}`;
+    },
   },
   linkedIn: {
     Icon: LinkedinIcon,
-    className: 'text-[#0077b5]',
+    className: 'text-secondary-3',
+    iconClassName: 'h-6 w-6',
+    getShareUrl: (text: string, url: string) => {
+      return `https://www.linkedin.com/shareArticle?url=${url}&title=${text}`;
+    },
+  },
+  hackerNews: {
+    Icon: HackerNewsIcon,
+    className: 'text-secondary-3',
+    iconClassName: 'h-5 w-5',
+    getShareUrl: (text: string, url: string) => {
+      return `https://news.ycombinator.com/submitlink?u=${url}&t=${text}`;
+    },
   },
 };
 
 export type SocialLink = {
   network: keyof typeof icons;
-  url: string;
+  text?: string;
+  url?: string;
 };
 
 type SocialLinksProps = {
@@ -28,17 +46,19 @@ const SocialLinks = ({ items }: SocialLinksProps) => {
   if (!items) return null;
 
   return (
-    <ul className="mt-4 flex gap-x-2">
-      {items.map(({ network, url }) => {
-        const { Icon, className } = icons[network];
+    <ul className="mt-2 flex gap-x-3">
+      {items.map(({ network, text, url }) => {
+        const { Icon, className, iconClassName, getShareUrl } = icons[network];
+        // If no url is provided, generate one from the text and current url.
+        const shareUrl = url || getShareUrl(text || document.title, url || location.href);
         return (
-          <li key={network}>
+          <li key={network} className="flex items-center justify-center ">
             <Link
               className={clsx(className, 'grayscale hover:grayscale-0')}
-              href={url}
-              aria-label={`Author on ${network}`}
+              href={shareUrl}
+              target="_blank"
             >
-              <Icon className="h-[26px] w-[26px]" role="presentation" />
+              <Icon className={clsx(iconClassName)} role="presentation" />
             </Link>
           </li>
         );
