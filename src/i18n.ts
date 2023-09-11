0e44f4d213b6048d5b823b403afecd6f85fc4a87
boojack <stevenlgtm@gmail.com>
2023-09-11 17:26:36 +0800

    chore: fix `t` usage (#178)
    * chore: fix `t` usage
    
    * chore: update

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,12 @@
 import { getRequestConfig } from 'next-intl/server';
 
-export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`./locales/${locale}.json`)).default,
-}));
+export async function getMessages(locale: string) {
+  return (await import(`./locales/${locale}.json`)).default;
+}
+
+export default getRequestConfig(async ({ locale }) => {
+  const messages = await getMessages(locale);
+  return {
+    messages,
+  };
+});
