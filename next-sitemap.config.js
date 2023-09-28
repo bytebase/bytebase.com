28fa9291cfe2a0534e5bb9ea888c018ea38859ac
Steven <stevenlgtm@gmail.com>
2023-09-28 19:13:20 +0800

    chore: update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,17 @@
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_DEFAULT_SITE_URL || 'https://www.bytebase.com',
   generateRobotsTxt: true,
+  // Custom the i18n path. Remove the prefix `en` for matched path.
+  // Reference: https://next-sitemap.iamvishnusankar.com/docs/documentation/custom-transformation
+  transform: async (config, path) => {
+    if (path.startsWith('/en')) {
+      path = path.substring(3);
+    }
+
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+    };
+  },
 };
