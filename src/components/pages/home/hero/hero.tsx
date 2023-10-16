6a12f02005e64b0cf5362683494a9e96979b07dd
Tianzhou Chen <t@bytebase.com>
2023-10-16 21:24:03 +0800

    chore: update what’s new

--- src/components/pages/home/hero/hero.tsx
+++ src/components/pages/home/hero/hero.tsx
@@ -134,14 +134,13 @@ const Hero = ({ latestVersion }: { latestVersion: { number: string; slug: string
       <section className="col-start-1 col-end-8 row-start-1 row-end-2 3xl:col-end-9 xl:col-end-10 md:col-end-12 sm:col-auto sm:row-auto">
         <header>
           {latestVersion !== null && (
-            <Link href={`${Route.CHANGELOG}/${latestVersion.slug}`} className="group/link">
+            <Link href="/blog/salla-case-study/" className="group/link">
               <span className="inline-flex items-center gap-1 rounded-full bg-tones-purple-light p-1 text-12 font-semibold leading-none text-primary-1 transition-colors duration-200 group-hover/link:text-primary-2">
                 <span className="rounded-full bg-primary-1 px-2 py-1 text-white transition-colors duration-200 group-hover/link:bg-primary-2">
                   What’s new?
                 </span>
                 <span className="flex items-center gap-1.5 px-2">
-                  Release {latestVersion.number} - Introduce Changelist to group, compose, apply or
-                  export database changes.
+                  Case study - Salla, the Shopify for Arab
                   <SmallArrowIcon width={7} height={6} />
                 </span>
               </span>
