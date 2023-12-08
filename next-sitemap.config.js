d9ff4c73a0cbd24c26dfe941feb1c52a6368e24e
tianzhou <t@bytebase.com>
2023-12-08 18:54:53 +0800

    chore: adjust page SEO priority

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,7 +5,7 @@ module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_DEFAULT_SITE_URL || 'https://www.bytebase.com',
   generateRobotsTxt: true,
   // Do not index duplicated zh content for now
-  exclude: ['/zh/*'],
+  exclude: ['/zh', '/zh/*'],
   // Custom the i18n path. Remove the prefix `en` for matched path.
   // Reference: https://next-sitemap.iamvishnusankar.com/docs/documentation/custom-transformation
   transform: (config, path) => {
@@ -18,10 +18,23 @@ module.exports = {
       path = '/';
     }
 
+    var priority = config.priority;
+    const components = path.split('/');
+    // Top level pathes having top priority
+    if (components.length <= 1) {
+      priority = 1.0;
+    } else if (path.startsWith('/blog/')) {
+      priority = 0.9;
+    } else if (path.startsWith('/docs/') || path.startsWith('/tutorial/')) {
+      priority = 0.7;
+    } else {
+      priority = 0.3;
+    }
+
     return {
       loc: path,
       changefreq: config.changefreq,
-      priority: config.priority,
+      priority: priority,
     };
   },
 };
