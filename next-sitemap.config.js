f056dceef328141bd0353abd643414aec8f0a90c
tianzhou <t@bytebase.com>
2023-12-08 18:28:30 +0800

    chore: do not index zh content for now

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,9 +4,10 @@ const DEFAULT_LANGUAGE = 'en';
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_DEFAULT_SITE_URL || 'https://www.bytebase.com',
   generateRobotsTxt: true,
+  // Do not index duplicated zh content for now
+  exclude: ['/zh/*'],
   // Custom the i18n path. Remove the prefix `en` for matched path.
   // Reference: https://next-sitemap.iamvishnusankar.com/docs/documentation/custom-transformation
-
   transform: (config, path) => {
     const defaultLanguageNeedle = '/' + DEFAULT_LANGUAGE;
     const defaultLanguageEnvelopeNeedle = defaultLanguageNeedle + '/';
