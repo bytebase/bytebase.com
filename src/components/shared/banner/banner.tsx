c8ff51b66e18665ffb1b6a9e43a05e61fc269540
Tianzhou Chen <t@bytebase.com>
2024-01-16 00:07:49 +0800

    chore: tweak banner style

--- src/components/shared/banner/banner.tsx
+++ src/components/shared/banner/banner.tsx
@@ -10,7 +10,7 @@ const Banner = ({ bannerText, bannerUrl }: { bannerText: string; bannerUrl: stri
         className="group/link relative -z-10 mx-auto flex h-14 w-full items-center justify-center px-4.5 md:h-[48px]"
         href={bannerUrl}
       >
-        <p className="text-center text-16 font-medium leading-tight transition-colors duration-200 line-clamp-1 md:text-14 2xs:text-left">
+        <p className="text-center text-18 leading-tight transition-colors duration-200 line-clamp-1 md:text-14 2xs:text-left">
           {bannerText}
         </p>
         <ArrowIcon className="ml-2 h-2.5 w-4 shrink-0 transition-transform duration-200 group-hover/link:translate-x-1 md:inline-block" />
