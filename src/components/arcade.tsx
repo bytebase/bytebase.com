0b9081b680eca3aeb1c0af4188e2fa18225f32a4
tianzhou <t@bytebase.com>
2024-01-22 15:49:14 +0800

    chore: demo

---
+++ src/components/arcade.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+export function Arcade() {
+  return (
+    <div className="relative flex h-0 w-full pb-[calc(75%+41px)]">
+      <iframe
+        src="https://demo.arcade.software/hyHeghfbclZ5NvaKTlVF?embed"
+        title="Database Change Workflow Embed"
+        frameBorder="0"
+        loading="lazy"
+        className="absolute inset-0 h-full w-full"
+        allowFullScreen
+      />
+    </div>
+  );
+}
