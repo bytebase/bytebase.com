4402b3c6392d9e59a9ae142bc0f28b3f2ee228fe
boojack <stevenlgtm@gmail.com>
2024-03-18 17:59:12 +0800

    chore: update dependencies (#302)
    * chore: update dependencies
    
    * chore: update
    
    * chore: update

---
+++ src/app/healthz/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  return NextResponse.json(
+    {
+      message: 'OK',
+    },
+    { status: 200 },
+  );
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -12,7 +12,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    '/((?!api|_next/static|.*\\..*|_next/image|favicon.ico|sitemap.xml|robots.txt|manifest.webmanifest).*)',
-  ],
+  matcher: ['/', '/(en|zh)/:path*'],
 };
