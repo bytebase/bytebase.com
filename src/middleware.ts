a7261a1a9933bad3f68f67f79ba2deb830f01865
boojack <stevenlgtm@gmail.com>
2024-03-18 18:12:24 +0800

    fix: revert matcher changes (#303)

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,5 +12,7 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/', '/(en|zh)/:path*'],
+  matcher: [
+    '/((?!api|_next/static|.*\\..*|_next/image|favicon.ico|sitemap.xml|robots.txt|manifest.webmanifest).*)',
+  ],
 };
