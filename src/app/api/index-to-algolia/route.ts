f4cc2e267ab3db432aa78f163f37177a0f5f7060
tianzhou <t@bytebase.com>
2024-03-18 19:02:42 +0800

    chore: include detailed server error

--- src/app/api/index-to-algolia/route.ts
+++ src/app/api/index-to-algolia/route.ts
@@ -191,6 +191,6 @@ export async function POST(request: NextRequest) {
       { status: 200 },
     );
   } catch (error) {
-    return NextResponse.json({ error: 'Server error' }, { status: 500 });
+    return NextResponse.json({ error: `Server error: ${error}` }, { status: 500 });
   }
 }
