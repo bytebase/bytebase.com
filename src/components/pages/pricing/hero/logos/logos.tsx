6fbf54bd2e25458a04ae6258c9d1740dcf0c6bd6
Tianzhou Chen <t@bytebase.com>
2024-03-24 13:46:01 +0800

    chore: tweak slogan

--- src/components/pages/pricing/hero/logos/logos.tsx
+++ src/components/pages/pricing/hero/logos/logos.tsx
@@ -5,9 +5,9 @@ const Logos = () => {
   return (
     <section className="container flex flex-col items-center">
       <h2 className="max-w-3xl text-center font-title text-56 font-semibold leading-none xl:max-w-2xl xl:text-56 md:max-w-lg md:text-48 sm:text-40">
-        Over 3,000,000 downloads worldwide
+        Fastest Growing Database and CI/CD Tool on GitHub
       </h2>
-      <ul className="mt-12 grid max-w-[1000px] grid-cols-4 gap-x-24 gap-y-12 2xl:mt-14 xl:mt-12 xl:max-w-[740px] xl:gap-y-8 xl:gap-x-16 md:mt-10 md:max-w-[660px] md:gap-y-7 md:gap-x-10 sm:mt-8 xs:flex xs:max-w-[460px] xs:flex-wrap xs:justify-center xs:gap-x-6 xs:gap-y-6">
+      <ul className="mt-12 grid max-w-[1000px] grid-cols-4 gap-x-24 gap-y-12 2xl:mt-14 xl:mt-12 xl:max-w-[740px] xl:gap-x-16 xl:gap-y-8 md:mt-10 md:max-w-[660px] md:gap-x-10 md:gap-y-7 sm:mt-8 xs:flex xs:max-w-[460px] xs:flex-wrap xs:justify-center xs:gap-x-6 xs:gap-y-6">
         {LOGO_LIST.map((logo) => (
           <li key={logo.alt} className="mx-auto flex items-center xs:mx-0">
             <Image
