38f72dabb4113130d51d123fdc831d3ce9d0ab21
tianzhou <t@bytebase.com>
2024-04-02 10:51:47 +0800

    chore: tweak banner slogan

--- src/lib/promo-data.ts
+++ src/lib/promo-data.ts
@@ -2,7 +2,7 @@ import ROUTE from '@/lib/route';
 
 const PROMO_DATA = {
   TOP_BANNER: {
-    title: 'Consolidate disparate DB tools. 10x efficiency, data security and PII compliance.',
+    title: 'All the Database Tools Bytebase Replaces',
     pathname: `/blog/all-database-tools-bytebase-replaces/?source=top-banner`,
   },
   BLOG_ASIDE: {
