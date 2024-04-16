880e59171ccff17a1e30a869b50d6e3e606fc4e6
adela <a@bytebase.com>
2024-04-16 12:31:33 +0800

    docs: extract shared components for tuts

--- src/components/shared/content/include-block/include-block.tsx
+++ src/components/shared/content/include-block/include-block.tsx
@@ -11,11 +11,16 @@ const getPost = (url: string) => {
   return getPostBySlug(docUrl);
 };
 
-const IncludeBlock = ({ url }: { url: string }) => {
+const IncludeBlock = ({ url, db }: { url: string; db: string }) => {
   const post = getPost(url);
 
   if (!post) {
     return null;
+  }
+  if (db) {
+    post.content = post.content.replaceAll('###db###', '-' + db);
+  } else {
+    post.content = post.content.replaceAll('###db###', '');
   }
 
   return <Content content={post.content} />;
