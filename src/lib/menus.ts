61e67cfc90d168217f36e8c37b0e152c614345e4
Tianzhou <t@bytebase.com>
2024-05-18 00:08:22 +0800

    docs: tweak emoji

--- src/lib/menus.ts
+++ src/lib/menus.ts
@@ -2,7 +2,7 @@ import Route from './route';
 
 export const MENU = {
   header: [
-    { title: 'Why Bytebase🤔', href: Route.DOCS },
+    { title: '🫱 Why Bytebase', href: Route.DOCS },
     {
       title: 'Features',
       items: [
@@ -87,7 +87,7 @@ export const MENU = {
     },
   ],
   mobile: [
-    { title: 'Why Bytebase🤔', href: Route.DOCS },
+    { title: '🫱 Why Bytebase', href: Route.DOCS },
     {
       title: 'Features',
       items: [
