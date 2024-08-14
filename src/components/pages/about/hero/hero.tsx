b82a81df56da556049e7aa392ca1bbf39c7b644b
tianzhou <t@bytebase.com>
2024-08-14 18:53:55 +0800

    chore: update about

--- src/components/pages/about/hero/hero.tsx
+++ src/components/pages/about/hero/hero.tsx
@@ -63,17 +63,17 @@ const Hero = () => {
           alt=""
           width={78}
           height={78}
-          className="absolute right-0 bottom-[38px] col-end-5 hidden sm:block"
+          className="absolute bottom-[38px] right-0 col-end-5 hidden sm:block"
         />
         <div className="absolute bottom-[355px] left-0 col-span-3 col-start-2 w-[266px] 3xl:w-auto xl:bottom-[328px] xl:col-span-4 xl:col-start-1 xl:w-[240px] md:bottom-[212px] md:col-span-3 md:col-start-1 md:w-auto sm:static sm:col-span-2 sm:mt-6 sm:text-center">
           <b className="font-title text-112 font-semibold leading-none xl:text-88 md:text-72 sm:text-48 sm:leading-95">
-            15
+            20+
           </b>
           <p className="leading-1.4 font-regular text-20 xl:mt-2 xl:text-16 xl:leading-snug md:text-14 md:leading-tight md:tracking-tight sm:mt-0">
             Bytebase supports every major database systems
           </p>
         </div>
-        <div className="absolute bottom-[327px] left-0 col-span-3 col-start-10 w-[284px] 3xl:w-auto xl:bottom-[303px] xl:right-0 xl:left-auto xl:col-span-4 xl:col-start-9 xl:w-[240px] md:bottom-[180px] md:col-span-3 md:col-start-10 md:w-full sm:static sm:col-span-2 sm:col-start-3 sm:mt-6 sm:text-center">
+        <div className="absolute bottom-[327px] left-0 col-span-3 col-start-10 w-[284px] 3xl:w-auto xl:bottom-[303px] xl:left-auto xl:right-0 xl:col-span-4 xl:col-start-9 xl:w-[240px] md:bottom-[180px] md:col-span-3 md:col-start-10 md:w-full sm:static sm:col-span-2 sm:col-start-3 sm:mt-6 sm:text-center">
           <b className="font-title text-112 font-semibold leading-none xl:text-88 md:text-72 sm:text-48 sm:leading-95">
             100+
           </b>
@@ -81,7 +81,7 @@ const Hero = () => {
             Bytebase offers the most comprehensive SQL Lint rules
           </p>
         </div>
-        <div className="absolute bottom-0 left-0 col-span-3 col-start-6 w-[252px] 3xl:w-auto xl:right-0 xl:left-auto xl:col-span-4 xl:col-start-5 xl:w-[240px] md:col-span-3 md:col-start-6 md:w-full sm:static sm:col-span-2 sm:col-start-2 sm:mt-7 sm:text-center">
+        <div className="absolute bottom-0 left-0 col-span-3 col-start-6 w-[252px] 3xl:w-auto xl:left-auto xl:right-0 xl:col-span-4 xl:col-start-5 xl:w-[240px] md:col-span-3 md:col-start-6 md:w-full sm:static sm:col-span-2 sm:col-start-2 sm:mt-7 sm:text-center">
           <b className="font-title text-112 font-semibold leading-none xl:text-88 md:text-72 sm:text-48 sm:leading-95">
             100%
           </b>
--- src/components/pages/about/meet-crew/founder/founder.tsx
+++ src/components/pages/about/meet-crew/founder/founder.tsx
@@ -19,7 +19,7 @@ const founder = {
       sm: '/images/page/about/danny-sm.webp',
     },
     about:
-      "Danny was the Staff Engineer at Google Cloud and TL overseeing Cloud SQL and API&nbsp;&&nbsp;Service Infrastructure. He has won Google's highest engineering award twice.",
+      "Danny was the Staff Engineer at Google Cloud and the tech lead overseeing Cloud SQL and API&nbsp;&&nbsp;Service Infrastructure. He has won Google's highest engineering award twice.",
   },
   tianzhou: {
     name: 'Tianzhou',
@@ -33,7 +33,7 @@ const founder = {
       sm: '/images/page/about/tianzhou-sm.webp',
     },
     about:
-      'Tianzhou was the TL of Google Cloud SQL, and the maintainer of Google&apos;s internal PostgreSQL / MySQL fork.',
+      'Tianzhou was the tech lead of Google Cloud SQL, and the maintainer of Google&apos;s internal PostgreSQL / MySQL fork.',
   },
 };
 
@@ -43,7 +43,7 @@ const Founder = ({ id, className }: { id: keyof typeof founder; className?: stri
   return (
     <article
       className={clsx(
-        'flex items-start border border-solid pt-8 pb-6 xl:pt-6 xl:pl-6 xl:pb-5 xl:pr-1 md:flex-col md:items-center md:p-6 md:pb-5 sm:items-start sm:pb-6',
+        'flex items-start border border-solid pb-6 pt-8 xl:pb-5 xl:pl-6 xl:pr-1 xl:pt-6 md:flex-col md:items-center md:p-6 md:pb-5 sm:items-start sm:pb-6',
         {
           'border-tones-blue-dark bg-tones-blue-light pl-8 pr-2 shadow-[inset_6px_6px_0_#fff,0_5px_15px_rgba(156,186,201,0.5)]':
             id === 'danny',
