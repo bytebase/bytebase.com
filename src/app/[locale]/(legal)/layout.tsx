1b03f7e6a74ee08716054c81c9a34dbf562995c3
Tianzhou <t@bytebase.com>
2024-08-27 00:27:08 +0700

    docs: tweak layout

--- src/app/[locale]/(legal)/layout.tsx
+++ src/app/[locale]/(legal)/layout.tsx
@@ -3,7 +3,7 @@ import SubscriptionForm from '@/components/shared/subscription';
 export default function LegalLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <section className="container gap-x-grid grid grid-cols-12 pt-[136px] 2xl:pt-32 lg:pt-[120px] md:pt-[104px] sm:pt-24">
+      <section className="container gap-x-grid grid grid-cols-12">
         <article className="content content-legal prose col-span-6 col-start-4 lg:col-span-8 lg:col-start-3 md:col-span-full md:max-w-full">
           {children}
         </article>
