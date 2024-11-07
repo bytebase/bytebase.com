641a8f33da39eca0438189d831ec9569d0b03694
adela <a@bytebase.com>
2024-11-07 14:38:36 +0800

    docs: add 3.0.1 changelog

--- src/components/pages/home/hero/hero.tsx
+++ src/components/pages/home/hero/hero.tsx
@@ -133,13 +133,13 @@ const Hero = ({ latestVersion }: { latestVersion: { number: string; slug: string
       <section className="col-start-1 col-end-8 row-start-1 row-end-2 3xl:col-end-9 xl:col-end-10 md:col-end-12 sm:col-auto sm:row-auto">
         <header>
           {latestVersion !== null && (
-            <Link href="/changelog/bytebase-3-0-0/" className="group/link">
+            <Link href="/changelog/bytebase-3-0-1/" className="group/link">
               <span className="inline-flex items-center gap-1 rounded-full bg-tones-purple-light p-1 text-12 font-semibold leading-none text-primary-1 transition-colors duration-200 group-hover/link:text-primary-2">
                 <span className="rounded-full bg-primary-1 px-2 py-1 text-white transition-colors duration-200 group-hover/link:bg-primary-2">
-                  Oct 24th, 2024
+                  Nov 7th, 2024
                 </span>
                 <span className="flex items-center gap-1.5 px-2">
-                  🚀 3.0.0: AI Assistant for SQL Editor
+                  🚀 3.0.1: Configurable DDL/DML Execution in SQL Editor
                   <SmallArrowIcon width={7} height={6} />
                 </span>
               </span>
