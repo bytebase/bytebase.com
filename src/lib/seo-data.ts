c98a831e19f3a62b6a62e25906767b698ea7424b
tianzhou <t@bytebase.com>
2024-11-11 11:38:06 +0800

    docs: tweak wording

--- src/lib/seo-data.ts
+++ src/lib/seo-data.ts
@@ -4,7 +4,7 @@ const SEO_DATA = {
   INDEX: {
     title: 'Database DevSecOps | Bytebase',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: '',
   },
   SCHEMA_MIGRATION: {
@@ -37,43 +37,43 @@ const SEO_DATA = {
   ABOUT: {
     title: 'About Us',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.ABOUT}/`,
   },
   PRICING: {
     title: 'Bytebase Pricing',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.PRICING}/`,
   },
   CHANGELOG: {
     title: 'Changelog',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.CHANGELOG}/`,
   },
   BLOG: {
     title: 'Blog',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.BLOG}/`,
   },
   TUTORIAL: {
     title: 'Bytebase Tutorial',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.TUTORIAL}/`,
   },
   DATABASE_GLOSSARY: {
     title: 'Database Glossary',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.DATABASE_GLOSSARY}/`,
   },
   TERMS: {
     title: 'Bytebase Terms of Service',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.TERMS}/`,
   },
   PARTNER: {
@@ -84,7 +84,7 @@ const SEO_DATA = {
   PRIVACY: {
     title: 'Bytebase Privacy Policy',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.PRIVACY}/`,
   },
   SECURITY: {
@@ -95,223 +95,223 @@ const SEO_DATA = {
   SLA: {
     title: 'Service Level Agreement',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.SLA}/`,
   },
   SQL_REVIEW_GUIDE: {
     title: 'Database Review Guide',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.SQL_REVIEW_GUIDE}/`,
   },
   DBA: {
     title: 'Bytebase for DBA',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.DBA}/`,
   },
   DEVELOPER: {
     title: 'Bytebase for Developer',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.DEVELOPER}/`,
   },
   TECHLEAD: {
     title: 'Bytebase for Tech Lead',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.TECHLEAD}/`,
   },
   CONTACTS: {
     title: 'Bytebase Contact',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.CONTACTS}/`,
   },
   REFUND: {
     title: 'Bytebase Refund',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.REFUND}/`,
   },
   VIEW_LIVE_DEMO: {
     title: 'View Live Demo',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.VIEW_LIVE_DEMO}/`,
   },
   REQUEST_DEMO: {
     title: 'Request a Demo',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.REQUEST_DEMO}/`,
   },
   CONFIRM_DEMO: {
     title: 'Demo Booking Confirmed',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.CONFIRM_DEMO}/`,
   },
   CONFIRM_VIEW_LIVE_DEMO: {
     title: 'Live Demo is Activated',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.CONFIRM_VIEW_LIVE_DEMO}/`,
   },
   CONFIRM_INQUIRY: {
     title: 'We Have Received Your Inquiry',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.CONFIRM_INQUIRY}/`,
   },
   CONFIRM_MESSAGE: {
     title: 'Your Message Has Been Sent',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.CONFIRM_MESSAGE}/`,
   },
   MYSQL: {
     title: 'Manage MySQL schema with Bytebase',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.DATABASE_MYSQL}/`,
   },
   POSTGRES: {
     title: 'Manage PostgreSQL schema with Bytebase',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.DATABASE_POSTGRES}/`,
   },
   CLICKHOUSE: {
     title: 'Manage ClickHouse schema with Bytebase',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.DATABASE_CLICKHOUSE}/`,
   },
   TIDB: {
     title: 'Manage TiDB schema with Bytebase',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.DATABASE_TIDB}/`,
   },
   SNOWFLAKE: {
     title: 'Manage Snowflake schema with Bytebase',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.DATABASE_SNOWFLAKE}/`,
   },
   ORACLE: {
     title: 'Manage Oracle schema with Bytebase',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.DATABASE_ORACLE}/`,
   },
   SQLSERVER: {
     title: 'Manage SQL Server schema with Bytebase',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.DATABASE_SQLSERVER}/`,
   },
   MONGO: {
     title: 'Manage MongoDB change management with Bytebase',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.DATABASE_MONGO}/`,
   },
   REDIS: {
     title: 'Manage Redis change management with Bytebase',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.DATABASE_REDIS}/`,
   },
   REDSHIFT: {
     title: 'Manage Redshift change management with Bytebase',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.DATABASE_REDSHIFT}/`,
   },
   MARIADB: {
     title: 'Manage MariaDB change management with Bytebase',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.DATABASE_MARIADB}/`,
   },
   OCEANBASE: {
     title: 'Manage OceanBase change management with Bytebase',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.DATABASE_OCEANBASE}/`,
   },
   SPANNER: {
     title: 'Manage Google Cloud Spanner change management with Bytebase',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.DATABASE_SPANNER}/`,
   },
   DATABRICKS: {
     title: 'Manage Databricks change management with Bytebase',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.DATABASE_DATABRICKS}/`,
   },
   GITLAB: {
     title: 'Integration with GitLab',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.INTEGRATION_GITLAB}/`,
   },
   GITHUB: {
     title: 'Integration with GitHub',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.INTEGRATION_GITHUB}/`,
   },
   BITBUCKET: {
     title: 'Integration with Bitbucket',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.INTEGRATION_BITBUCKET}/`,
   },
   AZURE_DEVOPS: {
     title: 'Integration with Azure DevOps',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.INTEGRATION_AZURE_DEVOPS}/`,
   },
   SLACK: {
     title: 'Integration with Slack',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.INTEGRATION_SLACK}/`,
   },
   DISCORD: {
     title: 'Integration with Discord',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.INTEGRATION_DISCORD}/`,
   },
   TEAMS: {
     title: 'Integration with Teams',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.INTEGRATION_TEAMS}/`,
   },
   DINGTALK: {
     title: 'Integration with DingTalk',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.INTEGRATION_DINGTALK}/`,
   },
   LARK: {
     title: 'Integration with Lark',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.INTEGRATION_LARK}/`,
   },
   WECOM: {
     title: 'Integration with WeCom',
     description:
-      'Database schema migration and version control, Database CI/CD, and DevOps for developers and DBAs',
+      'Schema migration and database security for developer, security, DBA, and platform engineering teams.',
     pathname: `${Route.INTEGRATION_WECOM}/`,
   },
   LIQUIBASE: {
