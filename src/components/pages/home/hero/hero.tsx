fd2d4de5c8042ed0f4dd745d1641c42becf1a53c
Adela <a@bytebase.com>
2024-12-05 14:47:31 +0800

    docs: add 3.1.1 (#480)

--- src/components/pages/home/hero/hero.tsx
+++ src/components/pages/home/hero/hero.tsx
@@ -134,13 +134,13 @@ const Hero = ({ latestVersion }: { latestVersion: { number: string; slug: string
       <section className="col-start-1 col-end-8 row-start-1 row-end-2 3xl:col-end-9 xl:col-end-10 md:col-end-12 sm:col-auto sm:row-auto">
         <header>
           {latestVersion !== null && (
-            <Link href="/changelog/bytebase-3-1-0/" className="group/link">
+            <Link href="/changelog/bytebase-3-1-1/" className="group/link">
               <span className="inline-flex items-center gap-1 rounded-full bg-tones-purple-light p-1 text-12 font-semibold leading-none text-primary-1 transition-colors duration-200 group-hover/link:text-primary-2">
                 <span className="rounded-full bg-primary-1 px-2 py-1 text-white transition-colors duration-200 group-hover/link:bg-primary-2">
-                  Nov 21st, 2024
+                  Dec 5th, 2024
                 </span>
                 <span className="flex items-center gap-1.5 px-2">
-                  🚀 3.1.0: Google/GitHub SSO available in Pro Plan
+                  🚀 3.1.1: Customizable homepage with shortcut list
                   <SmallArrowIcon width={7} height={6} />
                 </span>
               </span>
