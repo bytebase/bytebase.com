260eaaaa03726bc41df0746b03ca5ba959244050
adela <a@bytebase.com>
2025-01-02 14:49:13 +0800

    add 3.2.0 changelog

--- src/components/pages/home/hero/hero.tsx
+++ src/components/pages/home/hero/hero.tsx
@@ -14,17 +14,27 @@ import PromoCards from '../promo-cards';
 
 const Hero = ({ latestVersion }: { latestVersion: { number: string; slug: string } | null }) => {
   return (
-    <div className={clsx('container mt-16 flex w-full flex-col md:mt-8 sm:mt-4')}>
-      <section className="flex w-full flex-col items-center justify-center">
-        {latestVersion !== null && (
-          <Link href="/changelog/bytebase-3-1-2/" className="group/link">
-            <span className="inline-flex items-center gap-1 rounded-full bg-tones-purple-light p-1 text-12 font-semibold leading-none text-primary-1 transition-colors duration-200 group-hover/link:text-primary-2">
-              <span className="rounded-full bg-primary-1 px-2 py-1 text-white transition-colors duration-200 group-hover/link:bg-primary-2">
-                Dec 19th, 2024
-              </span>
-              <span className="flex items-center gap-1.5 px-2">
-                🚀 3.1.2: Tooltips for tables, columns, and PostgreSQL view comments in SQL Editor
-                <SmallArrowIcon width={7} height={6} />
+    <div
+      className={clsx(
+        'container gap-x-grid group mt-16 grid grid-cols-12 md:mt-8 sm:mt-4 sm:grid-cols-none',
+        {
+          done: isDone,
+        },
+      )}
+      ref={containerRef}
+    >
+      <section className="col-start-1 col-end-8 row-start-1 row-end-2 3xl:col-end-9 xl:col-end-10 md:col-end-12 sm:col-auto sm:row-auto">
+        <header>
+          {latestVersion !== null && (
+            <Link href="/changelog/bytebase-3-2-0/" className="group/link">
+              <span className="inline-flex items-center gap-1 rounded-full bg-tones-purple-light p-1 text-12 font-semibold leading-none text-primary-1 transition-colors duration-200 group-hover/link:text-primary-2">
+                <span className="rounded-full bg-primary-1 px-2 py-1 text-white transition-colors duration-200 group-hover/link:bg-primary-2">
+                  Jan 2nd, 2025
+                </span>
+                <span className="flex items-center gap-1.5 px-2">
+                  🚀 3.2.0: Selectively sync databases from an instance
+                  <SmallArrowIcon width={7} height={6} />
+                </span>
               </span>
             </span>
           </Link>
