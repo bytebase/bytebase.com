1b9c45034efd2ca95c49bb99bc4e9529cbef44c1
Tianzhou <t@bytebase.com>
2025-01-04 20:15:26 +0800

    docs: slogan highlight

--- src/components/pages/home/hero/hero.tsx
+++ src/components/pages/home/hero/hero.tsx
@@ -31,11 +31,10 @@ const Hero = ({ latestVersion }: { latestVersion: { number: string; slug: string
         )}
         <header className="relative mt-4 xl:mt-3.5 sm:mt-3">
           <h1 className="mx-auto mt-3 max-w-4xl text-center font-title text-108 font-semibold leading-none xl:text-80 xl:leading-none md:text-48 md:leading-none sm:text-48 sm:leading-95">
-            Database CI/CD and{' '}
+            Database CI/CD and Security{' '}
             <mark className="whitespace-nowrap bg-transparent text-center text-primary-1">
-              Security
-            </mark>{' '}
-            at Scale
+              at Scale
+            </mark>
           </h1>
         </header>
         <footer className="mt-12 flex items-center gap-9 2xl:gap-8 xl:mt-11 xl:gap-6 md:mt-7 sm:mt-6 sm:gap-3.5">
