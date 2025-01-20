ed2911fb20d91db2fac965eda1ae06beea0fb1c1
Tianzhou <t@bytebase.com>
2025-01-20 22:58:23 +0800

    chore: reduce comic

--- src/app/[locale]/blog/layout.tsx
+++ src/app/[locale]/blog/layout.tsx
@@ -1,12 +1,3 @@
-import Community from '@/components/shared/community';
-import SubscriptionForm from '@/components/shared/subscription';
-
 export default function BlogLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      {children}
-      <Community />
-      <SubscriptionForm />
-    </>
-  );
+  return <>{children}</>;
 }
--- src/app/[locale]/whitepaper/layout.tsx
+++ src/app/[locale]/whitepaper/layout.tsx
@@ -1,14 +1,9 @@
-import Community from '@/components/shared/community';
-import SubscriptionForm from '@/components/shared/subscription';
-
 export default function ContactLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <section className="bg-pricing-hero pb-20 pt-[136px] 2xl:pt-32 lg:pb-16 lg:pt-[120px] md:pb-12 md:pt-[104px] sm:pb-10 sm:pt-24">
         {children}
       </section>
-      <Community />
-      <SubscriptionForm />
     </>
   );
 }
