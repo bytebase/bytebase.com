0fbc94f277f8a3ff13b4a1d6a6503e5f82841274
Tianzhou <t@bytebase.com>
2025-02-09 23:00:14 +0800

    chore: update slack webhook again

--- src/app/api/slack/route.ts
+++ src/app/api/slack/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 const slackWebhookList = [
-  'https://hooks.slack.com/services/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',
+  'https://hooks.slack.com/services/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',
 ];
 
 export async function POST(request: Request) {
