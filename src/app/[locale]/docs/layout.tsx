ef843c731b2bdc634fa4eb868dfd66c9f93417a9
tianzhou <t@bytebase.com>
2025-02-11 14:24:09 +0800

    chore: remove subscription form from the docs page

--- src/app/[locale]/docs/layout.tsx
+++ src/app/[locale]/docs/layout.tsx
@@ -1,6 +1,5 @@
 import MobileSidebar from '@/components/pages/docs/mobile-sidebar';
 import Sidebar from '@/components/pages/docs/sidebar';
-import SubscriptionForm from '@/components/shared/subscription';
 
 import { getSidebar } from '@/lib/api-docs';
 
@@ -19,7 +18,7 @@ export default function DocLayout({ children }: { children: React.ReactNode }) {
       <div className="container grid grid-cols-12 gap-x-10 pt-8 xl:gap-x-9 lg:gap-x-6 md:mt-6 md:gap-x-5 sm:gap-x-4">
         <div
           className="sticky top-[144px] col-span-3 h-[calc(100vh-160px)] overflow-hidden 
-        before:absolute before:top-0 before:left-0 before:z-10 before:h-[72px] before:w-full before:bg-docs-sidebar 
+        before:absolute before:left-0 before:top-0 before:z-10 before:h-[72px] before:w-full before:bg-docs-sidebar 
         after:absolute after:bottom-0 after:left-0 after:z-10 after:h-10 after:w-full after:bg-gradient-to-b after:from-transparent after:to-white 
         md:hidden"
         >
@@ -28,7 +27,6 @@ export default function DocLayout({ children }: { children: React.ReactNode }) {
 
         {children}
       </div>
-      <SubscriptionForm />
     </>
   );
 }
