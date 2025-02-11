7844e0a3396c1b371294e6b16b30450f1c8b6def
tianzhou <t@bytebase.com>
2025-02-11 14:44:41 +0800

    chore: make docs page width align with the header

--- src/app/[locale]/docs/layout.tsx
+++ src/app/[locale]/docs/layout.tsx
@@ -15,17 +15,19 @@ export default function DocLayout({ children }: { children: React.ReactNode }) {
         data={sidebar}
         expandedList={expandedList}
       />
-      <div className="container grid grid-cols-12 gap-x-10 pt-8 xl:gap-x-9 lg:gap-x-6 md:mt-6 md:gap-x-5 sm:gap-x-4">
-        <div
-          className="sticky top-[144px] col-span-3 h-[calc(100vh-160px)] overflow-hidden 
-        before:absolute before:left-0 before:top-0 before:z-10 before:h-[72px] before:w-full before:bg-docs-sidebar 
-        after:absolute after:bottom-0 after:left-0 after:z-10 after:h-10 after:w-full after:bg-gradient-to-b after:from-transparent after:to-white 
-        md:hidden"
-        >
-          <Sidebar data={sidebar} expandedList={expandedList} />
-        </div>
+      <div className="mx-auto w-full max-w-[1920px] px-10 sm:px-4">
+        <div className="grid grid-cols-12 gap-x-10 pt-8 xl:gap-x-9 lg:gap-x-6 md:mt-6 md:gap-x-5 sm:gap-x-4">
+          <div
+            className="sticky top-[144px] col-span-3 h-[calc(100vh-160px)] overflow-hidden 
+          before:absolute before:left-0 before:top-0 before:z-10 before:h-[72px] before:w-full before:bg-docs-sidebar 
+          after:absolute after:bottom-0 after:left-0 after:z-10 after:h-10 after:w-full after:bg-gradient-to-b after:from-transparent after:to-white 
+          md:hidden"
+          >
+            <Sidebar data={sidebar} expandedList={expandedList} />
+          </div>
 
-        {children}
+          {children}
+        </div>
       </div>
     </>
   );
