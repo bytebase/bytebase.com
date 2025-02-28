dc22ec2ab4667c28ed765c0d207cc7e6c36a798e
tianzhou <t@bytebase.com>
2025-02-28 19:34:03 +0800

    fix: remove bad file

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import Resources from './pages/Resources';
-
-// Inside your Routes component
-<Routes>
-  // ... existing routes ...
-  <Route path="/resources" element={<Resources />} />
-  // ... existing routes ...
-</Routes>;
