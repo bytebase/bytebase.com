9a26a3fbee26e5a9aecf09cd4411da8947136a3b
tianzhou <t@bytebase.com>
2025-02-28 19:26:07 +0800

    chore: update banner

--- src/lib/promo-data.ts
+++ src/lib/promo-data.ts
@@ -1,22 +1,21 @@
-import ROUTE from '@/lib/route';
-
 const PROMO_DATA = {
   TOP_BANNER: {
     title: 'White Paper: Just-in-Time Database Access Best Practices',
     pathname: `/content/whitepaper/just-in-time-database-access/bytebase-whitepaper-just-in-time-database-access-best-practices.pdf`,
   },
   BLOG_ASIDE: {
-    title: 'What is Bytebase?',
+    title: 'All-in-One Database Workflows',
     description:
       'Schema migrations, adhoc changes, just-in-time (JIT) database access, and data masking in one place.',
-    cta: 'Learn more',
-    pathname: `/blog/all-database-tools-bytebase-replaces/?source=blog`,
+    cta: '⚙️ Unify now',
+    pathname: `/docs/introduction/what-is-bytebase/?source=blog`,
   },
   DOC_ASIDE: {
-    title: '🕹️ Live Demo',
-    description: 'We have prepared a guided live demo for you to play with.',
-    cta: 'View Now',
-    pathname: `${ROUTE.VIEW_LIVE_DEMO}?source=docs`,
+    title: ' All-in-One Database Workflows',
+    description:
+      'Schema migrations, adhoc changes, just-in-time (JIT) database access, and data masking in one place.',
+    cta: '⚙️ Unify now',
+    pathname: `/docs/introduction/what-is-bytebase/?source=docs`,
   },
 };
 
