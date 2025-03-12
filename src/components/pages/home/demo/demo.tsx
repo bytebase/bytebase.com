ebe0cfb0be3d05432e135955fe068c9c117d1502
tianzhou <t@bytebase.com>
2025-03-12 11:26:09 +0800

    chore: tweak interactive demo

--- src/components/pages/home/demo/demo.tsx
+++ src/components/pages/home/demo/demo.tsx
@@ -2,8 +2,11 @@ import { Arcade } from '@/components/arcade';
 
 const Demo = () => {
   return (
-    <section className="pt-24 2xl:pt-20 xl:pt-16 md:pt-12 sm:pt-4">
-      <div className="container flex w-full flex-col items-center">
+    <section className="pt-20 2xl:pt-16 xl:pt-12 md:pt-8 sm:pt-4">
+      <div className="container mb-16 flex w-full flex-col items-center space-y-20 2xl:space-y-16 xl:space-y-12 md:space-y-8 sm:space-y-4">
+        <h2 className="max-w-3xl text-center font-title text-72 font-semibold leading-none xl:max-w-2xl xl:text-56 md:max-w-lg md:text-48 sm:text-40">
+          Interactive Demo👇
+        </h2>
         <Arcade />
       </div>
     </section>
