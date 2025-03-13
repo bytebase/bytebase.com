ae369f1c5690f27653f18b292b9e389b6316f061
Adela <a@bytebase.com>
2025-03-13 17:27:52 +0800

    add 3.5.0 (#555)

--- src/components/pages/home/hero/hero.tsx
+++ src/components/pages/home/hero/hero.tsx
@@ -17,13 +17,13 @@ const Hero = ({ latestVersion }: { latestVersion: { number: string; slug: string
     <div className={clsx('container mt-16 flex w-full flex-col md:mt-8 sm:mt-4')}>
       <section className="flex w-full flex-col items-center justify-center">
         {latestVersion !== null && (
-          <Link href="/changelog/bytebase-3-4-1/" className="group/link">
+          <Link href="/changelog/bytebase-3-5-0/" className="group/link">
             <span className="inline-flex items-center gap-1 rounded-full bg-tones-purple-light p-1 text-12 font-semibold leading-none text-primary-1 transition-colors duration-200 group-hover/link:text-primary-2">
               <span className="rounded-full bg-primary-1 px-2 py-1 text-white transition-colors duration-200 group-hover/link:bg-primary-2">
-                Feb 27th, 2025
+                Mar 13th, 2025
               </span>
               <span className="flex items-center gap-1.5 px-2">
-                🚀 3.4.1: Terraform provider manages more resources
+                🚀 3.5.0: Azure IAM Auth for Cosmos DB & Azure SQL
                 <SmallArrowIcon width={7} height={6} />
               </span>
             </span>
