baf0f1b03198b9a447c6138fb055bfc9d845e3ef
Tianzhou <t@bytebase.com>
2025-04-04 15:20:45 +0800

    chore: adjust font

--- src/components/pages/home/promo-cards/card/card.tsx
+++ src/components/pages/home/promo-cards/card/card.tsx
@@ -36,7 +36,7 @@ const Card = forwardRef<HTMLElement, CardProps>(function CardComponent(
         />
       </div>
       <div className="mt-8">
-        <h3 className="overflow-hidden text-ellipsis whitespace-nowrap text-36 font-bold leading-extra-tight tracking-tighter xl:text-32 md:text-30 sm:text-24">
+        <h3 className="overflow-hidden font-title text-36 font-semibold xl:text-32 md:text-30 sm:text-24">
           {title}
         </h3>
         <p className="mt-4 w-3/4 text-20 leading-normal 3xl:w-full xl:relative xl:mt-3 xl:overflow-hidden xl:text-18 xl:after:absolute xl:after:bottom-0 xl:after:right-0 xl:after:h-[30px] xl:after:w-2/3 xl:after:bg-gradient-to-r xl:after:from-transparent xl:after:to-white lg:max-h-[81px] md:mt-1.5 md:max-h-[72px] md:text-16 sm:h-auto sm:leading-snug sm:after:hidden">
