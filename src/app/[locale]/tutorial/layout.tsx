bf35b3497ee8e84d0c92c96d0e1ed950fe3cf9d6
tianzhou <t@bytebase.com>
2025-04-14 15:04:53 +0800

    chore: remove subscription from news form

--- src/app/[locale]/tutorial/layout.tsx
+++ src/app/[locale]/tutorial/layout.tsx
@@ -1,10 +1,3 @@
-import SubscriptionForm from '@/components/shared/subscription';
-
 export default function TutorialLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      {children}
-      <SubscriptionForm />
-    </>
-  );
+  return <>{children}</>;
 }
