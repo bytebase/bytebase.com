a826c3a9f0c9b800b3b549c1d816255e217115bc
Tianzhou <t@bytebase.com>
2025-04-23 01:04:23 +0800

    chore: drop hero section margin

--- src/components/pages/home/hero/hero.tsx
+++ src/components/pages/home/hero/hero.tsx
@@ -12,16 +12,14 @@ import PromoCards from '../promo-cards';
 
 const Hero = () => {
   return (
-    <div className={clsx('container mt-16 flex w-full flex-col md:mt-8 sm:mt-4')}>
+    <div className={clsx('container mt-8 flex w-full flex-col md:mt-6 sm:mt-3')}>
       <section className="flex w-full flex-col items-center justify-center">
-        <header className="relative mt-4 xl:mt-3.5 sm:mt-3">
-          <h1 className="mx-auto mt-3 max-w-4xl text-balance text-center font-title text-108 font-semibold leading-none xl:text-80 xl:leading-none md:text-48 md:leading-none sm:text-48 sm:leading-95">
-            Database CI/CD and Security{' '}
-            <mark className="whitespace-nowrap bg-transparent text-center text-primary-1">
-              at Scale
-            </mark>
-          </h1>
-        </header>
+        <h1 className="mx-auto mt-3 max-w-4xl text-balance text-center font-title text-108 font-semibold leading-none xl:text-80 xl:leading-none md:text-48 md:leading-none sm:text-48 sm:leading-95">
+          Database CI/CD and Security{' '}
+          <mark className="whitespace-nowrap bg-transparent text-center text-primary-1">
+            at Scale
+          </mark>
+        </h1>
         <div className="mt-12 flex items-center gap-9 2xl:gap-8 xl:mt-11 xl:gap-6 md:mt-7 sm:mt-6 sm:gap-3.5">
           <Button href={Route.DEMO} theme="primary-filled" size="md" className="sm:!w-fit sm:!px-6">
             Try Demo
