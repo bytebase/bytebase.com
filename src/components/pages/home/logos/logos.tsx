5d88098419040dc3a605ea3e2a3c9a769b5c0958
Tianzhou <t@bytebase.com>
2025-04-23 00:31:41 +0800

    chore: shrink logo

--- src/components/pages/home/logos/logos.tsx
+++ src/components/pages/home/logos/logos.tsx
@@ -2,7 +2,7 @@ import { LANDING_LOGO_LIST } from '@/lib/logo-data';
 
 const Logos = () => {
   return (
-    <section className="mt-28 bg-[radial-gradient(50%_50%_at_50%_50%,rgba(255,255,255,0)34.28%,rgba(240,242,255,0.5)100%)] py-24 2xl:mt-24 2xl:py-20 xl:mt-20 xl:py-16 md:mt-12 md:py-12 sm:mt-8 sm:py-4">
+    <section className="mt-20 bg-[radial-gradient(50%_50%_at_50%_50%,rgba(255,255,255,0)34.28%,rgba(240,242,255,0.5)100%)] py-16 2xl:mt-16 2xl:py-16 xl:mt-14 xl:py-12 md:mt-10 md:py-10 sm:mt-6 sm:py-4">
       <div className="container mx-auto flex max-w-6xl flex-col items-center px-4">
         <ul className="grid w-full grid-cols-6 items-center gap-8 2xl:grid-cols-6 xl:grid-cols-6 lg:grid-cols-6 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-2">
           {LANDING_LOGO_LIST.map((logo) => (
