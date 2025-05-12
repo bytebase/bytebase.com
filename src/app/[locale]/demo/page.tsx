18abca9e11bc800e468807d1c0de1183aac966e3
tianzhou <t@bytebase.com>
2025-05-12 18:02:09 +0800

    chore: swap demo order

--- src/app/[locale]/demo/page.tsx
+++ src/app/[locale]/demo/page.tsx
@@ -12,15 +12,11 @@ export default function Page() {
   return (
     <section className="pt-20 2xl:pt-16 xl:pt-12 md:pt-8 sm:pt-4">
       <div className="container mb-16 flex w-full flex-col space-y-20 2xl:space-y-16 xl:space-y-12 md:space-y-8 sm:space-y-4">
-        <h2 className="max-w-3xl font-title text-72 font-semibold leading-none xl:max-w-2xl xl:text-56 md:max-w-lg md:text-48 sm:text-40">
-          Try Interactive Demo👇
-        </h2>
-        <Arcade />
         <div className="border-gray-200 flex items-center gap-8 border p-8 xl:gap-6 md:flex-col md:items-start md:gap-4">
           <div className="flex-grow">
             <p className="text-24 leading-normal xl:text-20 md:text-18">
-              Alternatively, you can view live demo or schedule a personalized walkthrough with our
-              team.
+              You can view live demo or schedule a personalized walkthrough with our team.
+              Alternatively, you can try the interactive demo below 👇👇👇.
             </p>
           </div>
           <div className="flex shrink-0 gap-4 md:w-full md:flex-col">
@@ -42,6 +38,7 @@ export default function Page() {
             </Button>
           </div>
         </div>
+        <Arcade />
       </div>
     </section>
   );
