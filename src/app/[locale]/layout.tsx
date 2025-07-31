9e57e0f1779d388d47c5fd03ac08d0d3419ad0e5
tianzhou <t@bytebase.com>
2025-07-31 10:19:15 +0800

    chore: swap analytics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -73,20 +73,11 @@ document.head.appendChild(o)}initApollo();
           }}
         />
         <Script
-          id="rb2b"
+          id="reo"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-!function () {var reb2b = window.reb2b = window.reb2b || [];
-if (reb2b.invoked) return;reb2b.invoked = true;reb2b.methods = ["identify", "collect"];
-reb2b.factory = function (method) {return function () {var args = Array.prototype.slice.call(arguments);
-args.unshift(method);reb2b.push(args);return reb2b;};};
-for (var i = 0; i < reb2b.methods.length; i++) {var key = reb2b.methods[i];reb2b[key] = reb2b.factory(key);}
-reb2b.load = function (key) {var script = document.createElement("script");script.type = "text/javascript";script.async = true;
-script.src = "https://s3-us-west-2.amazonaws.com/b2bjsstore/b/" + key + "/reb2b.js.gz";
-var first = document.getElementsByTagName("script")[0];
-first.parentNode.insertBefore(script, first);};
-reb2b.SNIPPET_VERSION = "1.0.1";reb2b.load("9NMMZHPVM0NW");}();
+      !function(){var e,t,n;e="91f044180a1552d",t=function(){Reo.init({clientID:"91f044180a1552d"})},(n=document.createElement("script")).src="https://static.reo.dev/"+e+"/reo.js",n.defer=!0,n.onload=t,document.head.appendChild(n)}();
 `,
           }}
         />
