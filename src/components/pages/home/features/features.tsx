4893655db600be114b875d8de545772acad94981
Tianzhou <t@bytebase.com>
2025-09-13 15:43:23 +0800

    docs: tweak feature card

--- src/components/pages/home/features/features.tsx
+++ src/components/pages/home/features/features.tsx
@@ -11,6 +11,12 @@ type TCard = {
 
 const cards: TCard[] = [
   {
+    href: Route.DOCS_AUDIT_LOG,
+    icon: '/images/attention.svg',
+    title: 'Audit Logging',
+    description: 'Complete audit trail of all database operations and schema changes.',
+  },
+  {
     href: Route.DOCS_ROLLBACK_DATA,
     icon: '/images/disk.svg',
     title: '1-Click Rollback',
@@ -22,12 +28,6 @@ const cards: TCard[] = [
     title: 'Schema Sync',
     description: 'Calculate and apply schema diff to make multiple databases consistent.',
   },
-  {
-    href: Route.DOCS_DRIFT_DETECTION,
-    icon: '/images/attention.svg',
-    title: 'Drift Detection',
-    description: 'Proactively detect schema drift caused by unexpected change.',
-  },
 ];
 
 const Features = () => {
@@ -35,11 +35,8 @@ const Features = () => {
     <section className="container mt-40 3xl:mt-36 xl:mt-32 md:mt-24 sm:mt-20">
       <header className="flex flex-col items-center text-center sm:items-start sm:text-start">
         <h2 className="font-title text-88 font-semibold leading-95 xl:text-68 xl:leading-104 md:text-54 sm:text-48">
-          Stay consistent
+          Monitor & Recover
         </h2>
-        <p className="mt-5 max-w-[716px] text-20 leading-normal 3xl:mt-[22px] xl:mt-5 xl:text-18 xl:leading-snug md:mt-3 md:max-w-[468px] md:text-14 sm:mt-2">
-          Continuous monitoring and 1-click rollback plan to protect your database 7x24.
-        </p>
       </header>
       <ul className="gap-x-grid mt-14 grid grid-cols-12 3xl:mt-12 xl:mt-11 md:mt-9 sm:mt-6 sm:gap-y-8">
         {cards.map((card, idx) => (
